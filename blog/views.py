b655cc24299c35f79837e38ff985b82f44c3d084
Andriy <dixionlink@gmail.com>
2025-05-21 01:35:16 +0300

    Added DRF API with Swagger docs for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,111 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
-# Create your views here.
+from drf_yasg.utils import swagger_auto_schema
+
+# --- POST CRUD ---
+class PostListCreate(APIView):
+    @swagger_auto_schema(
+        responses={200: PostSerializer(many=True)}
+    )
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(
+        request_body=PostSerializer,
+        responses={201: PostSerializer()}
+    )
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    @swagger_auto_schema(
+        responses={200: PostSerializer()}
+    )
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(
+        request_body=PostSerializer,
+        responses={200: PostSerializer()}
+    )
+    def put(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        responses={204: 'Deleted successfully'}
+    )
+    def delete(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# --- COMMENT CRUD ---
+class CommentListCreate(APIView):
+    @swagger_auto_schema(
+        responses={200: CommentSerializer(many=True)}
+    )
+    def get(self, request):
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(
+        request_body=CommentSerializer,
+        responses={201: CommentSerializer()}
+    )
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetail(APIView):
+    @swagger_auto_schema(
+        responses={200: CommentSerializer()}
+    )
+    def get(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(
+        request_body=CommentSerializer,
+        responses={200: CommentSerializer()}
+    )
+    def put(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        responses={204: 'Deleted successfully'}
+    )
+    def delete(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
